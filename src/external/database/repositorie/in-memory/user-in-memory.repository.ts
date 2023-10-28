01c792cf3c578cb91b87b98d286c09671db45114
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-10-28 08:06:24 -0300

    feat: implementing UserInMemoryRepository for UserEntity operations

---
+++ src/external/database/repositorie/in-memory/user-in-memory.repository.ts
@@ -0,0 +1,24 @@
+import { type UserRepository } from '../../../../adapter/gateways/repository.interface'
+import { type UserEntity } from '../../../../domain/entity/user/user.entity'
+import { ConflictError } from '../../../../shared/error/conflict-error'
+import { NotFoundError } from '../../../../shared/error/not-found-error'
+import { InMemorySearchableRepository } from '../../../../shared/util/repository/in-memory/user-in-memory-searchable.repository'
+
+export class UserInMemoryRepository
+  extends InMemorySearchableRepository<UserEntity>
+  implements UserRepository {
+  async findByEmail (email: string): Promise<UserEntity> {
+    const entity: UserEntity | undefined = this.items.find(item => item.email === email)
+    if (entity === undefined) {
+      throw new NotFoundError(`Entity not found using email ${email}`)
+    }
+    return entity
+  }
+
+  async emailExists (email: string): Promise<void> {
+    const entity: UserEntity | undefined = this.items.find(item => item.email === email)
+    if (entity !== undefined) {
+      throw new ConflictError('Email address already used')
+    }
+  }
+}
