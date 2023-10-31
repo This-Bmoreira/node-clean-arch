7d2ce7205db08d9b25d1c2d576ff39edada528d0
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-10-31 08:31:31 -0300

    refactor: fix import path in UserInMemoryRepository

--- src/external/database/repositorie/in-memory/user-in-memory.repository.ts
+++ src/external/database/repositorie/in-memory/user-in-memory.repository.ts
@@ -2,7 +2,7 @@ import { type UserRepository } from '../../../../adapter/gateways/repository.int
 import { type UserEntity } from '../../../../domain/entity/user/user.entity'
 import { ConflictError } from '../../../../shared/error/conflict-error'
 import { NotFoundError } from '../../../../shared/error/not-found-error'
-import { InMemorySearchableRepository } from '../../../../shared/util/repository/in-memory/user-in-memory-searchable.repository'
+import { InMemorySearchableRepository } from '../../../../shared/util/repository/in-memory/in-memory-searchable.repository'
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
