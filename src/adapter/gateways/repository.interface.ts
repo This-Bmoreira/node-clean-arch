514daf9cb79ef8b3ec147e9cbe56be26802800c8
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-11-02 07:43:17 -0300

    feat: restructure Repository Interfaces for Searchable Entities

--- src/adapter/gateways/repository.interface.ts
+++ src/adapter/gateways/repository.interface.ts
@@ -1,7 +1,4 @@
 import { type Entity } from '../../domain/entity/entity'
-import { type UserEntity } from '../../domain/entity/user/user.entity'
-import { type SearchParams } from '../../shared/util/repository/search-params/search-params'
-import { type SearchResult } from '../../shared/util/repository/search-params/search-result'
 
 export interface RepositoryInterface<E extends Entity> {
   insert: (entity: E) => Promise<void>
@@ -13,7 +10,7 @@ export interface RepositoryInterface<E extends Entity> {
 
 export type SortDirection = 'asc' | 'desc'
 
-export interface SearchProps<Filter = string> {
+export interface SearchParams<Filter = string> {
   page?: number
   perPage?: number
   sort?: string | null
@@ -21,7 +18,7 @@ export interface SearchProps<Filter = string> {
   filter?: Filter | null
 }
 
-export interface SearchResultProps<E extends Entity, Filter> {
+export interface SearchResult<E extends Entity, Filter> {
   items: E[]
   total: number
   currentPage: number
@@ -32,16 +29,11 @@ export interface SearchResultProps<E extends Entity, Filter> {
 }
 
 export interface SearchableRepositoryInterface<
-  E extends Entity,
-  searchInput = SearchParams,
+  E extends Entity<unknown>,
   Filter = string,
-  searchOutput = SearchResult<E, Filter>,
+  searchInput = SearchParams<Filter>,
+  searchOutput = SearchResult<E, Filter>
 > extends RepositoryInterface<E> {
   sortableFields: string[]
   search: (props: searchInput) => Promise<searchOutput>
-}
-
-export interface UserRepository extends SearchableRepositoryInterface<UserEntity, SearchParams, SearchResult<UserEntity, string>> {
-  findByEmail: (email: string) => Promise<UserEntity>
-  emailExists: (email: string) => Promise<void>
 }
