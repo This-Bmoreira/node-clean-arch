df47722c1d4ccea0e1e606884684794f0a3cc624
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-11-02 07:35:52 -0300

    feat: improve extensibility and apply dynamic sorting and filtering

--- src/external/database/repositorie/in-memory/user-in-memory.repository.ts
+++ src/external/database/repositorie/in-memory/user-in-memory.repository.ts
@@ -1,4 +1,5 @@
-import { type UserRepository } from '../../../../adapter/gateways/repository.interface'
+import { SortDirection } from '../../../../adapter/gateways/repository.interface'
+import { type UserRepository } from '../../../../adapter/gateways/user.repository'
 import { type UserEntity } from '../../../../domain/entity/user/user.entity'
 import { ConflictError } from '../../../../shared/error/conflict-error'
 import { NotFoundError } from '../../../../shared/error/not-found-error'
@@ -6,7 +7,18 @@ import { InMemorySearchableRepository } from '../../../../shared/util/repository
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository {
+  implements UserRepository.Repository {
+  protected async applyFilter (items: UserEntity[], filter: UserRepository.Filter): Promise<UserEntity[]> {
+    if (!filter) {
+      return items
+    }
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+
+  sortableFields: string[] = ['name', 'createdAt']
+
   async findByEmail (email: string): Promise<UserEntity> {
     const entity: UserEntity | undefined = this.items.find(item => item.email === email)
     if (entity === undefined) {
@@ -20,5 +32,15 @@ export class UserInMemoryRepository
     if (entity !== undefined) {
       throw new ConflictError('Email address already used')
     }
+  }
+
+  protected async applySort (
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? await super.applySort(items, 'createdAt', 'desc')
+      : await super.applySort(items, sort, sortDir)
   }
 }
